6c97a44b64c995f47bc2f39d0381d2c46bd53864
Kevin Marques De Souza <50784445+Kevinmsouza@users.noreply.github.com>
2022-01-21 21:51:53 -0400

    Removing .env.prod as a option on setup

--- src/setup.ts
+++ src/setup.ts
@@ -3,6 +3,5 @@ import dotenv from "dotenv";
 let path = ".env";
 
 if(process.env.NODE_ENV === "test") path = ".env.test";
-if(process.env.NODE_ENV === "production") path = ".env.prod";
 
 dotenv.config({ path });
