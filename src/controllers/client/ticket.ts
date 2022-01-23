fb039f8027c622409c825d8d1e39c78c8aee83f1
Kevinmsouza <kevinm.souza@hotmail.com>
2022-01-22 22:07:13 -0400

    fix: userId by body on get route

--- src/controllers/client/ticket.ts
+++ src/controllers/client/ticket.ts
@@ -3,13 +3,13 @@ import { Request, Response } from "express";
 import * as service from "@/services/client/ticket";
 
 export async function getTicketByUser(req: Request, res: Response) {
-  const { userId } = req.body;
+  const userId = req.user.id;
   const ticket = await service.getTicketByUser(userId);
   res.send(ticket);
 }
 
 export async function updateTicket(req: Request, res: Response) {
-  const { userId } = req.body;
+  const userId = req.user.id;
   const tickedPaid = await service.updateTicket(userId);
   res.send(tickedPaid);
 }
